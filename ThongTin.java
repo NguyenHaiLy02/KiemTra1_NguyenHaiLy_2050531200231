f2521a2052b759f5afe9ba985188de2936ca4872
NguyenHaiLy02 <100576881+NguyenHaiLy02@users.noreply.github.com>
2022-04-08 09:52:07 +0700

    Add files via upload

---
+++ ThongTin.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author win
+ */
+public class ThongTin {
+    private String hoTen31, maSV31, ngaySinh31,queQuan31,chucVu31;
+    public ThongTin() {
+
+    }
+
+    public ThongTin(String hoTen31, String maSV31,String ngaySinh31,String queQuan31,String chucVu31) {
+        this.hoTen31 = hoTen31;
+        this.maSV31 = maSV31;
+        this.ngaySinh31 = ngaySinh31;
+        this.queQuan31 = queQuan31;
+        this.chucVu31 = chucVu31;
+    }
+
+    /**
+     * @return the hoTen31
+     */
+    public String getHoTen31() {
+        return hoTen31;
+    }
+
+    /**
+     * @param hoTen31 the hoTen31 to set
+     */
+    public void setHoTen31(String hoTen31) {
+        this.hoTen31 = hoTen31;
+    }
+
+    /**
+     * @return the maSV31
+     */
+    public String getMaSV31() {
+        return maSV31;
+    }
+
+    /**
+     * @param maSV31 the maSV31 to set
+     */
+    public void setMaSV31(String maSV31) {
+        this.maSV31 = maSV31;
+    }
+
+    /**
+     * @return the ngaySinh31
+     */
+    public String getNgaySinh31() {
+        return ngaySinh31;
+    }
+
+    /**
+     * @param ngaySinh31 the ngaySinh31 to set
+     */
+    public void setNgaySinh31(String ngaySinh31) {
+        this.ngaySinh31 = ngaySinh31;
+    }
+
+    /**
+     * @return the queQuan31
+     */
+    public String getQueQuan31() {
+        return queQuan31;
+    }
+
+    /**
+     * @param queQuan31 the queQuan31 to set
+     */
+    public void setQueQuan31(String queQuan31) {
+        this.queQuan31 = queQuan31;
+    }
+
+    /**
+     * @return the chucVu31
+     */
+    public String getChucVu31() {
+        return chucVu31;
+    }
+
+    /**
+     * @param chucVu31 the chucVu31 to set
+     */
+    public void setChucVu31(String chucVu31) {
+        this.chucVu31 = chucVu31;
+    }
+
+    /**
+     * @return the hoTen
+     */
+    
+}
